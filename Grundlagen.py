22086fa34e578e98c931c2d621509a5fbac45d94
ShumilaMalik <shamila.malik@student.techstarter.de>
2023-03-16 11:33:14 +0100

    put reverse no.

--- Grundlagen.py
+++ Grundlagen.py
@@ -1,6 +1,6 @@
 
 # Dies ist ein einzeiliger Kommentar
-print("Hallo, Welt!") # Ein Kommentar kann auch am Ende einer Codezeile stehen
+#print("Hallo, Welt!") # Ein Kommentar kann auch am Ende einer Codezeile stehen
 
 """
 Dies ist ein mehrzeiliger Kommentar.
@@ -20,11 +20,17 @@ VARIABLEN
 Variablen ermöglichen uns Werte zu speichern und sie abzurufen
 """
 
-a = 3
-b = 5
+def reverse_digits(n):
+    # Convert the number to a string and reverse it
+    reversed_str = str(n)[::-1]
+    
+    # Convert each character back to an integer and store it in a list
+    reversed_digits = [int(char) for char in reversed_str]
+    
+    return reversed_digits
 
-name = "shumi"
-age = 40
-kid = 3
-pet = "cat"
-print(a+b)
+# Example usage:
+number = 12345
+result = reverse_digits(number)
+print(result)  # Output: [5, 4, 3, 2, 1]
+
