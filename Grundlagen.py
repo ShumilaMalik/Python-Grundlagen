98ca00397696432c9a6b77d8084c935702248c93
ShumilaMalik <shamila.malik@student.techstarter.de>
2023-03-17 11:53:00 +0100

    ok

--- Grundlagen.py
+++ Grundlagen.py
@@ -20,17 +20,4 @@ VARIABLEN
 Variablen ermöglichen uns Werte zu speichern und sie abzurufen
 """
 
-def reverse_digits(n):
-    # Convert the number to a string and reverse it
-    reversed_str = str(n)[::-1]
-    
-    # Convert each character back to an integer and store it in a list
-    reversed_digits = [int(char) for char in reversed_str]
-    
-    return reversed_digits
-
-# Example usage:
-number = 12345
-result = reverse_digits(number)
-print(result)  # Output: [5, 4, 3, 2, 1]
 
